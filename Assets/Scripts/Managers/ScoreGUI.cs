a4e0c799e501fe31ce200894397f4b03fd73da08
Julien Osouf <Julien Osouf>
2018-03-20 14:35:31 +0100

    menu without load game

--- Assets/Scripts/Managers/ScoreGUI.cs
+++ Assets/Scripts/Managers/ScoreGUI.cs
@@ -1,5 +1,6 @@
 ﻿using System.Diagnostics;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ScoreGUI : MonoBehaviour
 {
@@ -20,6 +21,12 @@ public class ScoreGUI : MonoBehaviour
         scoreUI += "<b>Targets hit</b>" + " : " + m_Tank.m_TargetsKilled + "\n" + "<b>Time</b> : " + m_Time.ElapsedMilliseconds / 1000 + "s";
 
         GUI.Label(new Rect(10, Screen.height - 40, 100, 50), scoreUI);
+
+        bool exitButton = GUI.Button(new Rect(Screen.width - 102, Screen.height - 42, 100, 40), "Exit");
+        string sceneToLoad = "Menu";
+
+        // If we click on the exit button we go to the menu
+        if (exitButton) SceneManager.LoadScene(sceneToLoad);
     }
 
     public void Update()
