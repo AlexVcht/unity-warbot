b447b4f8eca4c6d665d671c71b99898ceaf75f4f
Julien Osouf <Julien Osouf>
2018-03-20 16:18:56 +0100

    load game

--- Assets/Scripts/Managers/ButtonManager.cs
+++ Assets/Scripts/Managers/ButtonManager.cs
@@ -1,10 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class ButtonManager : MonoBehaviour {
     public void NewGame(string newGameLevel)
+    {
+        File.Delete(Application.persistentDataPath + "/SaveState.gd");
+        UnityEngine.Debug.Log("Deleted SaveState : " + Application.persistentDataPath + "/SaveState.gd");
+        SceneManager.LoadScene(newGameLevel);
+    }
+
+    public void LoadGame(string newGameLevel)
     {
         SceneManager.LoadScene(newGameLevel);
     }
